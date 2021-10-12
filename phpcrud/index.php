769c5d5bc9783a137e268b59efadd2eb47d760b7
khuszaimahazizah <80659481+khuszaimahazizah@users.noreply.github.com>
2021-10-12 14:58:30 +0700

    Add files via upload

---
+++ phpcrud/index.php
@@ -0,0 +1,24 @@
+<?php
+include 'functions.php';
+// Your PHP code here.
+
+// Home Page template below.
+?>
+<head>
+	<link rel="icon" href="image/Logo_polkam.png" type="image/ico" />
+<?=template_header('Web Data Mahasiswaa Politeknik Kampar')?>
+
+<div class="content">
+	<h2>Home</h2>
+	<p align="center" >Web site ini dibuat untuk membantu proses pendataan mahasiswa politeknik kampar</p>
+	<img src="image/bg2.JPG" style= "background-color: white;
+			align: center;
+			width: 100%;
+			margin;
+			padding: 0px;">
+
+	
+</div>
+
+<?=template_footer()?>
+</head>
---
+++ phpcrud/update.php
@@ -0,0 +1,56 @@
+<?php
+include 'functions.php';
+$pdo = pdo_connect_mysql();
+$msg = '';
+
+if (isset($_GET['id'])) {
+    if (!empty($_POST)) {
+        
+        $id = isset($_POST['id']) ? $_POST['id'] : NULL;
+        $nama = isset($_POST['nama']) ? $_POST['nama'] : '';
+        $email = isset($_POST['email']) ? $_POST['email'] : '';
+        $notelp = isset($_POST['notelp']) ? $_POST['notelp'] : '';
+        $pekerjaan = isset($_POST['prodi']) ? $_POST['prodi'] : '';
+        
+        // Update
+        $stmt = $pdo->prepare('UPDATE kontak SET id = ?, nama = ?, email = ?, notelp = ?, pekerjaan = ? WHERE id = ?');
+        $stmt->execute([$id, $nama, $email, $notelp, $prodi, $_GET['id']]);
+        $msg = 'Updated Successfully!';
+    }
+    // kontak
+    $stmt = $pdo->prepare('SELECT * FROM kontak WHERE id = ?');
+    $stmt->execute([$_GET['id']]);
+    $contact = $stmt->fetch(PDO::FETCH_ASSOC);
+    if (!$contact) {
+        exit('Contact doesn\'t exist with that ID!');
+    }
+} else {
+    exit('No ID specified!');
+}
+?>
+
+
+
+<?=template_header('Read')?>
+
+<div class="content update">
+	<h2>Update Contact #<?=$contact['id']?></h2>
+    <form action="update.php?id=<?=$contact['id']?>" method="post">
+        <label for="id">ID</label>
+        <label for="nama">Nama</label>
+        <input type="text" name="id" value="<?=$contact['id']?>" id="id">
+        <input type="text" name="nama" value="<?=$contact['nama']?>" id="nama">
+        <label for="email">Email</label>
+        <label for="notelp">No. Telp</label>
+        <input type="text" name="email" value="<?=$contact['email']?>" id="email">
+        <input type="text" name="notelp" value="<?=$contact['notelp']?>" id="notelp">
+        <label for="pekerjaan">prodi</label>
+        <input type="text" name="prodi" value="<?=$contact['prodi']?>" id="title">
+        <input type="submit" value="Update">
+    </form>
+    <?php if ($msg): ?>
+    <p><?=$msg?></p>
+    <?php endif; ?>
+</div>
+
+<?=template_footer()?>
